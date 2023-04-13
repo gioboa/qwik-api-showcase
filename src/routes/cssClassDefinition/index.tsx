208a8b9f857d2c4515cbf1a39dc3da01bc9ec9b3
gioboa <giorgiob.boa@gmail.com>
2023-04-13 09:01:08 +0200

    add cssClassDefinition

---
+++ src/routes/cssClassDefinition/index.tsx
@@ -0,0 +1,31 @@
+import { component$, useSignal } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  const randomNumberSignal = useSignal<number>(Math.random);
+  return (
+    <div class="flex flex-col items-center">
+      <div
+        class={{
+          "my-4 w-[300px] h-[300px]": true,
+          "bg-red-600": randomNumberSignal.value > 0.5,
+          "bg-blue-600": randomNumberSignal.value <= 0.5,
+          "bg-green-600": randomNumberSignal.value <= 0.3,
+        }}
+      />
+      <button onClick$={() => (randomNumberSignal.value = Math.random())}>
+        Change color
+      </button>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Qwik APIs Showcase",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik APIs Showcase",
+    },
+  ],
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,43 +1,46 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-	return (
-		<div class='flex flex-col items-center'>
-			<div class='my-4'>
-				<a href='/useTask'>useTask$</a>
-			</div>
-			<div class='my-4'>
-				<a href='/useTaskAndTrack'>useTask$ and track</a>
-			</div>
-			<div class='my-4'>
-				<a href='/useTaskWithDelay'>useTask$ with delay</a>
-			</div>
-			<div class='my-4'>
-				<a href='/useComputed'>useComputed$</a>
-			</div>
-			<div class='my-4'>
-				<a href='/useVisibleTask'>useVisibleTask$</a>
-			</div>
-			<div class='my-4'>
-				<a href='/server$'>server$</a>
-			</div>
-			<div class='my-4'>
-				<a href='/routeLoader'>routeLoader$</a>
-			</div>
-			<div class='my-4'>
-				<a href='/routeAction'>routeAction$</a>
-			</div>
-		</div>
-	);
+  return (
+    <div class="flex flex-col items-center">
+      <div class="my-4">
+        <a href="/cssClassDefinition">cssClassDefinition</a>
+      </div>
+      <div class="my-4">
+        <a href="/useTask">useTask$</a>
+      </div>
+      <div class="my-4">
+        <a href="/useTaskAndTrack">useTask$ and track</a>
+      </div>
+      <div class="my-4">
+        <a href="/useTaskWithDelay">useTask$ with delay</a>
+      </div>
+      <div class="my-4">
+        <a href="/useComputed">useComputed$</a>
+      </div>
+      <div class="my-4">
+        <a href="/useVisibleTask">useVisibleTask$</a>
+      </div>
+      <div class="my-4">
+        <a href="/server$">server$</a>
+      </div>
+      <div class="my-4">
+        <a href="/routeLoader">routeLoader$</a>
+      </div>
+      <div class="my-4">
+        <a href="/routeAction">routeAction$</a>
+      </div>
+    </div>
+  );
 });
 
 export const head: DocumentHead = {
-	title: 'Qwik APIs Showcase',
-	meta: [
-		{
-			name: 'description',
-			content: 'Qwik APIs Showcase',
-		},
-	],
+  title: "Qwik APIs Showcase",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik APIs Showcase",
+    },
+  ],
 };
